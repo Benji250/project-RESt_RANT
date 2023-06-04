7930f8cacd740c36daf4301163d4a483911f47c1
Benji250 <123209910+Benji250@users.noreply.github.com>
2023-06-03 19:42:50 -0700

    New places can be added

---
+++ models/places.js
@@ -0,0 +1,15 @@
+module.exports = [{
+name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'public/images/lily-banse--YHSwy6uqvk-unsplash.jpg'
+      }, {
+        name: 'Coding Dog Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'public/images/charlesdeluvio-_p-DIj5LJKc-unsplash.jpg'
+      }]
+   
+ 
