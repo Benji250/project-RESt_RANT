34c0af9ed88d4d172e39f61524cdaa794f1ac517
Benji250 <123209910+Benji250@users.noreply.github.com>
2023-06-06 19:32:20 -0700

    Added the edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -53,20 +53,33 @@ router.post('/', (req,res) =>{
     })
 
     //SHOW
-    router.get('/:id', (req, res) => {
-      res.render('places/edit')
+    router.put('/:id', (req, res) => {
       let id = Number(req.params.id)
-      if (isNaN(id)){
-        res.render('error404')
+      if (isNaN(id)) {
+          res.render('error404')
       }
-      else if (!places[id]){
-          res.render(error404)
+      else if (!places[id]) {
+          res.render('error404')
       }
       else {
-        places.splice(id, 1)
-        res.redirect('/places/edit', { place: places[id] })
+          // Dig into req.body and make sure data is valid
+          if (!req.body.pic) {
+              // Default image if one is not provided
+              req.body.pic = 'http://placekitten.com/400/400'
+          }
+          if (!req.body.city) {
+              req.body.city = 'Anytown'
+          }
+          if (!req.body.state) {
+              req.body.state = 'USA'
+          }
+    
+          // Save the new data into places[id]
+          places[id] = req.body
+          res.redirect(`/places/${id}`)
       }
     })
+    
 
 
 module.exports = router
