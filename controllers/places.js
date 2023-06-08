467e64fc39b49e9d1bcde157a8cea8f4a7813cce
Benji250 <123209910+Benji250@users.noreply.github.com>
2023-06-08 12:50:22 -0700

    Updated my places.js controllers

--- controllers/places.js
+++ controllers/places.js
@@ -1,87 +1,90 @@
-const router = require('express').Router()
-const places = require ('../modles/places.js')
+const express = require("express");
+const router = express.Router();
+const places = require("../models/places.js");
 
-//NEW
-router.post('/new' (req, res) => {
-  res.render('places/new')
-})
-
-//POST
-router.post('/', (req,res) =>{
-  // console.log(req.body)
-  console.log(req.body)
+router.post("/", (req, res) => {
+  console.log(req.body);
   if (!req.body.pic) {
-    req.body.pic = 'http://placekitten.com/400/400'
+    req.body.pic = "http://placekitten.com/400/400";
   }
   if (!req.body.city) {
-    req.body.city = 'Anytown'
+    req.body.city = "Anytown";
   }
   if (!req.body.state) {
-    req.body.state = 'USA'
+    req.body.state = "USA";
   }
-    places.push(req.body)
-   res.redirect('/places')
-      let id = Number(req.params.id)
-      if (isNaN(id)){
-        res.render('error404')
-      }
-      else if (!places[id]){
-          res.render(error404)
-      }
-      else {
-        places.splice(id, 1)
-        res.redirect('/places')
-      }
-    })
-    // let places = [{
-    //     name: 'H-Thai-ML',
-    //     city: 'Seattle',
-    //     state: 'WA',
-    //     cuisines: 'Thai, Pan-Asian',
-    //     pic: 'public/images/lily-banse--YHSwy6uqvk-unsplash.jpg'
-    //   }, {
-    //     name: 'Coding Dog Cafe',
-    //     city: 'Phoenix',
-    //     state: 'AZ',
-    //     cuisines: 'Coffee, Bakery',
-    //     pic: 'public/images/charlesdeluvio-_p-DIj5LJKc-unsplash.jpg'
-    //   }]
+  places.push(req.body);
+  res.redirect("/places");
+});
 
-    //INDEX
-    router.get('/', (req, res) => {
-      res.render('places/index', {places})
-    })
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
 
-    //SHOW
-    router.put('/:id', (req, res) => {
-      let id = Number(req.params.id)
-      if (isNaN(id)) {
-          res.render('error404')
-      }
-      else if (!places[id]) {
-          res.render('error404')
-      }
-      else {
-          // Dig into req.body and make sure data is valid
-          if (!req.body.pic) {
-              // Default image if one is not provided
-              req.body.pic = 'http://placekitten.com/400/400'
-          }
-          if (!req.body.city) {
-              req.body.city = 'Anytown'
-          }
-          if (!req.body.state) {
-              req.body.state = 'USA'
-          }
-    
-          // Save the new data into places[id]
-          places[id] = req.body
-          res.redirect(`/places/${id}`)
-      }
-    })
-    
+//show
+router.get("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/show", { place: places[id], id });
+  }
+});
+//delete
+router.delete("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    places.splice(id, 1);
+    res.redirect("/places");
+  }
+});
+//Edit
+router.get("/:id/edit", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/edit", { place: places[id], id: id });
+  }
+});
 
+router.put("/:id", (req, res) => {
+  let id = Number(req.params.id);
 
-module.exports = router
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    // Dig into req.body and make sure data is valid
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = "http://placekitten.com/400/400";
+    }
+    if (!req.body.city) {
+      req.body.city = "Anytown";
+    }
+    if (!req.body.state) {
+      req.body.state = "USA";
+    }
 
+    // Save the new data into places[id]
+    places[id] = req.body;
+    res.redirect(`/places/${id}`);
+  }
+});
 
+//   GET /places
+router.get("/", (req, res) => {
+  res.render("places/index", { places });
+});
+
+module.exports = router;
