8b984ee1ed727738358faba5a5aa6fe0a3960016
Benji250 <123209910+Benji250@users.noreply.github.com>
2023-06-20 14:23:39 -0700

    worked on part 10 and fixed some bugs

--- controllers/places.js
+++ controllers/places.js
@@ -12,6 +12,28 @@ router.get('/', (req, res) => {
   })
 })
 
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
+})
+
+
 router.post('/', (req, res) => {
   db.Place.create(req.body)
   .then(() => {
@@ -38,9 +60,12 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+//Show
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
+  .populate('comments')
   .then(place => {
+    console.log(place.comments)
     res.render('places/show', { place })
   })
   .catch(err => {
@@ -68,6 +93,37 @@ router.post('/:id/rant', (req, res) => {
 router.delete('/:id/rant/:rantId', (req, res) => {
     res.send('GET /places/:id/rant/:rantId stub')
 })
+
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+    db.Comment.create(req.body)
+    .then(comment => {
+      place.comments.push(comment.id)
+      place.save()
+      .then(() => {
+        res.redirect(`/places/${req.params.id}`)
+      })
+      //Todo: save comment id to place
+    })
+    .catch(err => {
+      res.render('error404')
+    })
+  })
+  .catch(err => {
+    res.render('error404')
+  })
+  // if (req.body.rant) {
+  //   req.body.rant = true
+  // }
+  // else {
+  //   req.body.rant = false
+  // }
+  // req.body.rant = req.body.rant ? true : false
+  res.send('GET /places/:id/comment stub')
+})
+
 
 module.exports = router
 
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,14 +2,33 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
-    let message = ""
-    if (data.message) {
-        message = (
-            <h4 className="alert-dnager">
-                {data.message}
-            </h4>
-        )
-    }
+    let comments = (
+        <h3 className='inactive'>
+            No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+          return (
+            <div className="border">
+              <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+              <h4>{c.content}</h4>
+              <h3>
+                <stong>- {c.author}</stong>
+              </h3>
+              <h4>Rating: {c.stars}</h4>
+            </div>
+          )
+        })
+      }
+    // let message = ""
+    // if (data.message) {
+    //     message = (
+    //         <h4 className="alert-dnager">
+    //             {data.message}
+    //         </h4>
+    //     )
+    // }
     return (
         <Def>
           <main>
@@ -25,6 +44,7 @@ function show (data) {
                 <h2>
                     Comments
                 </h2>
+                {comments}
                 <p>No Comments Yet!</p>
             </div>
             <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
